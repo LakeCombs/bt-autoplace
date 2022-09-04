920113a58c212fc3c88b6192692a607680eeec1a
Orinameh <memefacto@yahoo.com>
2022-09-04 20:48:24 +0100

    clean up for production: WIP

--- pages/api/seed.js
+++ pages/api/seed.js
@@ -1,6 +1,6 @@
 import nc from "next-connect";
 import Product from "../../models/Product";
-import data from "../../utils/data";
+// import data from "../../utils/data";
 import db from "../../utils/db";
 import User from '../../models/User';
 
@@ -9,9 +9,9 @@ const handler = nc();
 handler.get(async (req, res) => {
   await db.connect();
   await User.deleteMany();
-  await User.insertMany(data.users);
+  // await User.insertMany(data.users);
   await Product.deleteMany();
-  await Product.insertMany(data.products);
+  // await Product.insertMany(data.products);
   await db.disconnect();
   res.send({ message: "seeded successfully" });
 });
